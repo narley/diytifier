1004596a2ea8b1a277ae571e30d8aa036d673f3d
Narley Brittes <narley@brittes.co.uk>
2020-02-04 21:53:50 +0000

    feat: load config either from .env or environment variables

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,18 @@ module Main where
 
 import           Configuration.Dotenv (defaultConfig, loadFile)
 import           Lib
+import           System.Directory
+import           System.Environment
 
 main :: IO ()
 main = do
-  loadFile defaultConfig
-
+  hasDotEnv <- doesFileExist ".env"
+  if hasDotEnv then
+    putStrLn "Loading config from .env file..."
+    >> loadFile defaultConfig
+  else
+    putStrLn "Loading config from environment variables..."
+    >> getEnvironment
   dates <- getStartDates
   if hasNewCourses dates then
     sendEmailNotification $ getNewCourse dates
